569371150f2390333c52325633e150e5a94af4fa
h.nakahara <nakahara@coremind.jp>
2013-01-20 01:21:17 +0900

    add
    added accessor method "skew", "baseEase"

--- coremind/motion/easingOption/BackOption.js
+++ coremind/motion/easingOption/BackOption.js
@@ -1,25 +1,20 @@
-cm.Class.create({
+cm.Class.create(
+{
     $name:"cm.motion.easingOption.BackOption",
-    $static:
-    {
-        baseEase:function (p) { return p*p; },
-        skew:1.70158
-    },
     $define:
     {
         BackOption:function(baseEase, skew)
         {
-            this.baseEase = cm.equal.isFunction(baseEase) ?
-                baseEase:
-                cm.motion.easingOption.ElasticOption.baseEase;
-            
-            this.skew = cm.equal.isNumber(skew) ?
-                skew:
-                cm.motion.easingOption.ElasticOption.skew;
+            this.mBaseEase = cm.equal.isFunction(baseEase) ? baseEase: function (p) { return p*p; };
+            this.mSkew = cm.equal.isNumber(skew) ? skew: 1.70158;
         },
         destroy:function(){},
         
-        skewFix:function(val) { this.skew  = val; },
-        skewRel:function(val) { this.skew += val; }
+        baseEase:function() { return this.mBaseEase; },
+        setBaseEase:function(easing) { this.mBaseEase = easing; },
+        
+        skew:function(val) { this.mSkew  = val; },
+        skewAbs:function(val) { this.mSkew  = val; },
+        skewRel:function(val) { this.mSkew += val; }
     }
 });
